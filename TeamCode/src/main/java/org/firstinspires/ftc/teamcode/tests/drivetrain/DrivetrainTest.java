e900ad3a0f3667ef392ed2624f4b1b312498c775
Jeffrey Jiang <43833907+MechaDL7@users.noreply.github.com>
2019-08-11 15:33:28 -0400

    Update DrivetrainTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/drivetrain/DrivetrainTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/drivetrain/DrivetrainTest.java
@@ -1,17 +1,17 @@
 package org.firstinspires.ftc.teamcode.tests.drivetrain;
-​
+
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-​
+
 import org.firstinspires.ftc.teamcode.sensors.Gyroscope;
 import org.firstinspires.ftc.teamcode.subsystems.Drivetrain;
 import org.firstinspires.ftc.teamcode.util.Constants;
 import org.firstinspires.ftc.teamcode.util.MotionTracker;
-​
+
 import java.util.Arrays;
-​
+
 @TeleOp(name = "Drivetrain Test", group = "Drivetrain")
 public class DrivetrainTest extends OpMode {
     private Drivetrain drive;
@@ -21,7 +21,7 @@ public class DrivetrainTest extends OpMode {
     private DcMotor leftBack;
     private DcMotor rightFront;
     private DcMotor rightBack;
-​
+
     @Override
     public void init() {
         drive = new Drivetrain(hardwareMap);
@@ -35,18 +35,18 @@ public class DrivetrainTest extends OpMode {
         rightFront.setDirection(DcMotor.Direction.REVERSE);
         rightBack.setDirection(DcMotor.Direction.REVERSE);
     }
-​
+
     @Override
     public void loop() {
         double FrontLeftVal =  gamepad1.left_stick_y - (gamepad1.left_stick_x)  + -gamepad1.right_stick_x;
         double FrontRightVal =  gamepad1.left_stick_y  + (gamepad1.left_stick_x) - -gamepad1.right_stick_x;
         double BackLeftVal = gamepad1.left_stick_y  + (gamepad1.left_stick_x)  + -gamepad1.right_stick_x;
         double BackRightVal = gamepad1.left_stick_y - (gamepad1.left_stick_x) - -gamepad1.right_stick_x;
-​
+
         //Move range to between 0 and +1, if not already
         double[] wheelPowers = {FrontRightVal, FrontLeftVal, BackLeftVal, BackRightVal};
         Arrays.sort(wheelPowers);
-​
+
         if (wheelPowers[3] > 1) {
             FrontLeftVal /= wheelPowers[3];
             FrontRightVal /= wheelPowers[3];
