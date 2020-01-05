a23fee23c030918dcb9ee7e625fed3fcea9d9822
Jeffrey Jiang <43833907+MechaDL7@users.noreply.github.com>
2020-01-05 14:19:30 -0500

    Update Arm.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Arm.java
@@ -16,15 +16,21 @@ public class Arm extends Subsystem {
     private double assumePowerE;
     private double assumePowerR;
 
-    private double limitEU;
-    private double limitED;
-    private double limitRU;
-    private double limitRD;
+    private double limitEU = 10000;
+    private double limitRU = 10000;
+    private double limitED = -10000;
+    private double limitRD = -10000;
+
+    private boolean grabState = false;
     public Arm(HardwareMap map) {
         motorExtend = map.dcMotor.get(Constants.Arm.MOTOR_EXTEND);
         motorRotate = map.dcMotor.get(Constants.Arm.MOTOR_ROTATE);
         servoBack = map.servo.get(Constants.Arm.SERVO_BACK);
         servoGrab = map.servo.get(Constants.Arm.SERVO_GRAB);
+        motorRotate.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorExtend.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorExtend.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorRotate.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
 
     @Override
@@ -34,21 +40,35 @@ public class Arm extends Subsystem {
 
     @Override
     public void loop() {
-        if(motorExtend.getCurrentPosition() > limitED && motorExtend.getCurrentPosition() < limitEU){
+        if(motorExtend.getCurrentPosition() >= limitED && motorExtend.getCurrentPosition() <= limitEU){
             motorExtend.setPower(assumePowerE);
         }else{
             motorExtend.setPower(0);
         }
-        if(motorRotate.getCurrentPosition() > limitRD && motorRotate.getCurrentPosition() < limitRU){
+        if(motorRotate.getCurrentPosition() >= limitRD && motorRotate.getCurrentPosition() <= limitRU){
             motorRotate.setPower(assumePowerR);
         }else{
             motorRotate.setPower(0);
         }
+
+        if(grabState){
+            servoGrab.setPosition(Servo.MAX_POSITION);
+        }else{
+            servoGrab.setPosition(Servo.MIN_POSITION);
+        }
+
     }
     public void setMotorPowerE(double a){
         assumePowerE = a;
     }
     public void setMotorPowerR(double a){
         assumePowerR = a;
+    }
+
+    public void setGrabState(boolean a){
+        grabState = a;
+    }
+    public boolean getGrabState(){
+        return grabState;
     }
 }
