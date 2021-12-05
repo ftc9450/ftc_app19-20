34788eb0002b687bb99b7a72741854b267d535be
Jeffrey Jiang <43833907+MechaDL7@users.noreply.github.com>
2021-12-05 16:41:24 -0500

    Update ServoTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/servo/ServoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/servo/ServoTest.java
@@ -5,7 +5,9 @@ import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.qualcomm.hardware.motors.NeveRest20Gearmotor;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.configuration.typecontainers.MotorConfigurationType;
 
@@ -26,18 +28,21 @@ public class ServoTest extends OpMode {
     //private List<Double> wheelVelo;
     private Servo servo1;
     private Servo servo2;
+    private CRServo cr1;
     private DcMotor motor;
 
 
 
     @Override
     public void init() {
-        servo1 = hardwareMap.servo.get("foundServoRight");
-
+        cr1 = hardwareMap.crservo.get("foundServoRight");
+        cr1.setDirection(DcMotorSimple.Direction.FORWARD);
     }
 
     @Override
     public void loop() {
-       servo1.setPosition(gamepad1.left_stick_y);
+        cr1.setPower(gamepad1.left_stick_y);
+        telemetry.addData("Servo",cr1.getPower());
+        telemetry.update();
     }
 }
